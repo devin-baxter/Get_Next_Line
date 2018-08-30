df1c57e81d43c76338a99b99862f32ebc26a586b
Devin Baxter <debaxter@e1z3r6p24.42.us.org>
2018-08-30 15:13:27 -0700

    GNL save

--- get_next_line.c
+++ get_next_line.c
@@ -12,8 +12,70 @@
 
 #include "get_next_line.h"
 
+char	*ft_read_line (char **gnl, char *buf, int fd)
+{
+	char	*temp;
+	int		rd_fd;
+
+	rd_fd = 1;
+	while (!(ft_strchr(*gnl, '\n')) && rd_fd)
+	{
+		rd_fd = read(fd, buf, BUFF_SIZE);
+		if (rd_fd)
+		{
+			buf[rd_fd] = '\0';
+			temp = *gnl;
+			if (!(*gnl = ft_strjoin(*gnl, buf)))
+				return (NULL);
+			free(temp);
+		}
+	}
+	free(buf);
+	return (*gnl);
+}
+
+char	*ft_stock_line(char **gnl)
+{
+	char	*buffer;
+	char	*new_line;
+	char	*temp;
+
+	buffer = ft_strchr(*gnl, '\n');
+	temp = NULL;
+	if (buffer)
+	{
+		if (!(new_line = ft_strndup(*gnl, buffer - *gnl)))
+			return (NULL);
+		temp = *gnl;
+		if (!(*gnl = ft_strdup(buffer + 1)))
+			return (NULL);
+		free(temp);
+	}
+	else if (!(new_line = ft_strdup(*gnl)))
+		return (NULL);
+	if (!(*gnl) || !temp)
+	{
+		free(*gnl);
+		*gnl = NULL;
+	}
+	return (new_line);
+}
+
 int		get_next_line(const int fd, char **line)
 {
-	char	*filename;
-	char	buf[BUFF_SIZE];
+	static char	*gnl;
+	char		*buf;
+
+	*gnl = NULL;
+	if (NO_FILE || NO_BUFF || NO_READ || NO_LINE)
+		return (-1);
+	if (!(ft_read_line(&gnl, buf, fd)))
+		return (-1);
+	if (*gnl)
+	{
+		if (!(*line = ft_stock_line(&gnl)))
+			return (-1);
+		return (1);
+	}
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -9,14 +9,23 @@
 /*   Updated: 2018/07/07 12:53:59 by debaxter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include "libft/libft.h"
 
 # define BUFF_SIZE 64
+# define NO_FILE (fd < 0 || !line);
+# define NO_BUFF (BUFF_SIZE <= 0 || !(buf = ft_strnew(BUFF_SIZE +1)));
+# define NO_READ (read(fd, buf, 0) == -1);
+# define NO_LINE (gnl == NULL && !(gnl = ft_strnew(0)));
 
 int		get_next_line(const int fd, char **line);
+char	*ft_read_line (char **gnl, char *buf, int fd);
+char	*ft_stock_line(char **gnl);
 
 #endif
